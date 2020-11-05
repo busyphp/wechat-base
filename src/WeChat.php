17749591a464036ff818d6556786b268fd16bfa9
刘晓彬 <563859671@qq.com>
2020-11-05 19:29:47 +0800

    增加配置类

--- src/WeChat.php
+++ src/WeChat.php
@@ -14,6 +14,8 @@ use BusyPHP\Request;
  */
 abstract class WeChat
 {
+    use WeChatConfig;
+    
     /**
      * @var App
      */
@@ -27,21 +29,7 @@ abstract class WeChat
     
     public function __construct()
     {
-        $this->app = app();
-        $this->app->config->load($this->app->getRootPath() . 'config' . DIRECTORY_SEPARATOR . 'extend' . DIRECTORY_SEPARATOR . 'wechat.php', 'wechat');
+        $this->app     = app();
         $this->request = $this->app->request;
-    }
-    
-    
-    /**
-     * 获取配置
-     * @param string $name 配置名称
-     * @param mixed  $default 默认值
-     * @return mixed
-     * @deprecated 该方法已不建议试用，参考新方法 {@see WeChatConfig::getConfig()}
-     */
-    public function getConfig(string $name, $default = null)
-    {
-        return $this->app->config->get('wechat.' . $name, $default);
     }
 }
