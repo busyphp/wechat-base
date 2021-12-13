64dd528263205e0836f3b770c1438838792c008c
刘晓彬 <563859671@qq.com>
2021-12-13 09:09:13 +0800

    调整配置

--- src/WeChatConfig.php
+++ src/WeChatConfig.php
@@ -12,24 +12,19 @@ use BusyPHP\App;
  */
 trait WeChatConfig
 {
-    private static $isLoadConfig = false;
-    
-    
     /**
      * 获取配置
      * @param string $name 配置名称
      * @param mixed  $default 默认值
      * @return mixed
      */
-    public function getConfig(string $name, $default = null)
+    public function getWeChatConfig(string $name = '', $default = null)
     {
         $app = App::getInstance();
-        if (!self::$isLoadConfig) {
+        if (!$app->config->get('wechat', null)) {
             $app->config->load($app->getRootPath() . 'config' . DIRECTORY_SEPARATOR . 'extend' . DIRECTORY_SEPARATOR . 'wechat.php', 'wechat');
-            
-            self::$isLoadConfig = true;
         }
         
-        return $app->config->get('wechat.' . $name, $default);
+        return $app->config->get('wechat' . ($name ? ".{$name}" : ''), $default);
     }
 }
